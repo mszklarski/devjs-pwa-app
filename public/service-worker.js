2f6201e654a0cff082172ee19008239ec9bbd8bf
Michal Szklarski <mszklarski@e-point.pl>
2020-06-27 12:37:17 +0200

    basic serviceWorker

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,10 +3,18 @@
 const CACHE_NAME = 'static-cache-v1';
 
 const FILES_TO_CACHE = [
+  '/offline.html',
 ];
 
 self.addEventListener('install', (evt) => {
   console.log('[ServiceWorker] Install');
+
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline page');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  )
 
   self.skipWaiting();
 });
