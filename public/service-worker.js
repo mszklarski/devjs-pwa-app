8af508b5b280f604443a367c0099a53cdd96239f
Michal Szklarski <mszklarski@e-point.pl>
2020-06-27 12:57:36 +0200

    basic serviceWorker with offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -22,10 +22,35 @@ self.addEventListener('install', (evt) => {
 self.addEventListener('activate', (evt) => {
   console.log('[ServiceWorker] Activate');
 
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }))
+    })
+  )
+
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (evt) => {
   console.log('[ServiceWorker] Fetch', evt.request.url);
+
+  if (evt.request.mode !== 'navigate') {
+    return;
+  }
+
+  evt.respondWith(
+    fetch(evt.request)
+    .catch(() => {
+      return caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.match('offline.html');
+      })
+    })
+  );
 
 });
